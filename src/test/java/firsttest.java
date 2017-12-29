bd968e64f00c19612934eee589c8f0cdd2d13c78
Boris Golomb <boris_golomb@epam.com>
2017-12-29 18:18:10 +0300

    Initial commit

---
+++ src/test/java/firsttest.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.Test;
+
+public class firsttest {
+
+    @Test
+    public void testSrcCreatingBo(){
+
+        System.setProperty("webdriver.gecko.driver", "./src/test/resources/geckodriver.exe");
+        System.setProperty("webdriver.firefox.logfile", "/dev/null");
+        FirefoxOptions options = new FirefoxOptions();
+        WebDriver driver = new FirefoxDriver(options);
+        driver.get("http://evbyminsd2156.minsk.epam.com/logon/logonServlet");
+        driver.findElement(By.xpath("//input[@name='j_username']")).sendKeys("pb-001");
+        driver.findElement(By.xpath("//input[@name='j_password']")).sendKeys("Support-2");
+        driver.findElement(By.xpath("(//div[@class='login-button']/a)[1]")).click();
+
+
+        driver.close();
+    }
+
+}
+
+
+
+
