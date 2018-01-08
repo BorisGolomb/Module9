abb972aa657e461dcca94df2578aff82a9db9a8d
Boris Golomb <boris_golomb@epam.com>
2018-01-08 17:49:20 +0300

    add one step

--- src/test/java/firsttest.java
+++ src/test/java/firsttest.java
@@ -21,6 +21,8 @@ public class firsttest {
         driver.findElement(By.xpath("//input[@name='j_username']")).sendKeys("pb-001");
         driver.findElement(By.xpath("//input[@name='j_password']")).sendKeys("Support-2");
         driver.findElement(By.xpath("(//div[@class='login-button']/a)[1]")).click();
+        driver.get("http://evbyminsd2156.minsk.epam.com/sourcing");
+
 
 
         driver.close();
