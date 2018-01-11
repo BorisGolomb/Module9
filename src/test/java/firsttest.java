6acd301639beec0c3eed09247253b830efdfef65
Boris Golomb <boris_golomb@epam.com>
2018-01-11 18:46:23 +0300

    add some step

--- src/test/java/firsttest.java
+++ src/test/java/firsttest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
@@ -6,7 +7,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class firsttest {
 
@@ -17,15 +25,23 @@ public class firsttest {
         System.setProperty("webdriver.firefox.logfile", "/dev/null");
         FirefoxOptions options = new FirefoxOptions();
         WebDriver driver = new FirefoxDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("http://evbyminsd2156.minsk.epam.com/logon/logonServlet");
-        driver.findElement(By.xpath("//input[@name='j_username']")).sendKeys("pb-001");
-        driver.findElement(By.xpath("//input[@name='j_password']")).sendKeys("Support-2");
+        driver.findElement(By.xpath("//input[@name='j_username']")).sendKeys("ivan_500");
+        driver.findElement(By.xpath("//input[@name='j_password']")).sendKeys("Support-1");
         driver.findElement(By.xpath("(//div[@class='login-button']/a)[1]")).click();
-        driver.get("http://evbyminsd2156.minsk.epam.com/sourcing");
+        List<WebElement> elements = driver.findElements(By.cssSelector("a.navBarLogoutLink"));
+        //Assert.assertEquals(elements.size(), 2, "failed to login");
+        //driver.get("http://evbyminsd2156.minsk.epam.com/sourcing");
+        WebElement element = driver.findElement(By.xpath("//div[text()='SupplyOn Services']/ancestor::li[@class='navBarTopMenuItemContainer']"));
+        new Actions(driver).moveToElement(element).build().perform();
+        driver.findElement(By.xpath("//DIV[@templateid='navbarSubmenuItemText'][text()='Action Management']")).click();
+        driver.findElement(By.xpath("//DIV[@id='innerTopics:j_id113:j_id125_span']"));
 
 
 
-        driver.close();
+       // new WebDriverWait(driver, 15).until(ExpectedConditions.);
+       // driver.close();
     }
 
 }
