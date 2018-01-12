65fb19a25a3c244411be1c67bd3b8d2eb3194c15
Boris Golomb <boris_golomb@epam.com>
2018-01-12 18:09:13 +0300

    add some step

--- src/test/java/firsttest.java
+++ src/test/java/firsttest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -18,13 +19,15 @@ import java.util.concurrent.TimeUnit;
 
 public class firsttest {
 
+    WebDriver driver;
+
     @Test
     public void testSrcCreatingBo(){
 
         System.setProperty("webdriver.gecko.driver", "./src/test/resources/geckodriver.exe");
         System.setProperty("webdriver.firefox.logfile", "/dev/null");
         FirefoxOptions options = new FirefoxOptions();
-        WebDriver driver = new FirefoxDriver(options);
+        driver = new FirefoxDriver(options);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("http://evbyminsd2156.minsk.epam.com/logon/logonServlet");
         driver.findElement(By.xpath("//input[@name='j_username']")).sendKeys("ivan_500");
@@ -35,8 +38,16 @@ public class firsttest {
         //driver.get("http://evbyminsd2156.minsk.epam.com/sourcing");
         WebElement element = driver.findElement(By.xpath("//div[text()='SupplyOn Services']/ancestor::li[@class='navBarTopMenuItemContainer']"));
         new Actions(driver).moveToElement(element).build().perform();
-        driver.findElement(By.xpath("//DIV[@templateid='navbarSubmenuItemText'][text()='Action Management']")).click();
-        driver.findElement(By.xpath("//DIV[@id='innerTopics:j_id113:j_id125_span']"));
+        driver.findElement(By.xpath("//div[@templateid='navbarSubmenuItemText'][text()='Action Management']")).click();
+        WebElement myDynamicElement = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//div[contains(@class,'am-create-from-template-dropdown')]/div)[1]")));
+        myDynamicElement.click();
+
+        new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'bgo-t-1610-1')]"))).click();
+        driver.findElement(By.xpath("//input[@id='markup_inner:incl:topic_name']")).sendKeys("selenium_test");
+        driver.findElement(By.xpath("//label[contains(text(), 'Description')]/ancestor::td/following-sibling::td//textarea")).sendKeys("selenium_test");
+
 
 
 
@@ -44,6 +55,10 @@ public class firsttest {
        // driver.close();
     }
 
+    //@AfterClass
+   // public void tearDown(){
+     //   driver.close();
+    //}
 }
 
 
